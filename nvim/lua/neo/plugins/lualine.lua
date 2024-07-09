0fe43fd3b3be0acfa906872d59d9afaed3df9c81
Nivesh Birangal <niveshbirangal@gmail.com>
2024-07-09 23:29:52 +0530

    add logo to status line

--- nvim/lua/neo/plugins/lualine.lua
+++ nvim/lua/neo/plugins/lualine.lua
@@ -57,12 +57,13 @@ return {
       sections = {
         lualine_x = {
           {
+
             lazy_status.updates,
             cond = lazy_status.has_updates,
             color = { fg = "#ff9e64" },
           },
           { "encoding" },
-          { "fileformat" },
+          { "fileformat", symbols = { unix = "" } },
           { "filetype" },
         },
       },
