7d3bcd1d63934f46b327c6d8e5c55eadd9e8dd44
Nivesh Birangal <niveshbirangal@gmail.com>
2024-07-12 00:14:31 +0530

    update templates

--- nvim/lua/neo/core/templates.lua
+++ nvim/lua/neo/core/templates.lua
@@ -1,5 +1,5 @@
 local keymap = vim.keymap
 -- shortcuts
 -- keymap.set("i", "clog", "console.log()")
-keymap.set("i", "CL", "console.log()<ESC>j", { desc = "console.log shortcut" })
-keymap.set("i", "CF", "function temp(){}<ESC>j<INSERT>", { desc = "console.log shortcut" })
+keymap.set("i", "CL", "console.log()<ESC>h", { desc = "console.log shortcut" })
+keymap.set("i", "CF", "function temp(){}<ESC>h", { desc = "console.log shortcut" })
