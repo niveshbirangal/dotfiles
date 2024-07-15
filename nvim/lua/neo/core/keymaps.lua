6901d6e7810262dc11d3a5dee09f4b267ecbf8c6
Nivesh Birangal <niveshbirangal@gmail.com>
2024-07-16 00:57:26 +0530

    add fast swap windows and save keymaps

--- nvim/lua/neo/core/keymaps.lua
+++ nvim/lua/neo/core/keymaps.lua
@@ -18,7 +18,9 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>ss", "<C-w>x", { desc = "swap windows" }) -- swap windows
 
+-- tabs
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
@@ -38,3 +40,6 @@ keymap.set("v", "leader<p>", "_dP")
 -- experimental
 keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- select text and move down
 keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- select text and move up
+
+-- save
+keymap.set("n", "<leader>ww", "<cmd>w<CR>", { desc = "save current file" }) -- save current file
