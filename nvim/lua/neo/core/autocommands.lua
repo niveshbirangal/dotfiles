e8a65440921757e4fa20a9af0e8a30b94e9e6068
Nivesh Birangal <niveshbirangal@gmail.com>
2024-07-25 23:13:46 +0530

    move status line down

--- nvim/lua/neo/core/autocommands.lua
+++ nvim/lua/neo/core/autocommands.lua
@@ -10,6 +10,29 @@ api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove({ "r", "o" })
   end,
 })
+-- end disable continuous comments
+
+-- decrease status line space
+-- vim.opt.cmdheight = 1
+api.nvim_create_autocmd("CmdlineEnter", {
+  group = api.nvim_create_augroup("cmdheight_1_on_cmdlineenter", { clear = true }),
+  desc = "Don't hide the status line when typing a command",
+  command = ":set cmdheight=1",
+})
+
+api.nvim_create_autocmd("CmdlineLeave", {
+  group = api.nvim_create_augroup("cmdheight_0_on_cmdlineleave", { clear = true }),
+  desc = "Hide cmdline when not typing a command",
+  command = ":set cmdheight=0",
+})
+
+api.nvim_create_autocmd("BufWritePost", {
+  group = api.nvim_create_augroup("hide_message_after_write", { clear = true }),
+  desc = "Get rid of message after writing a file",
+  pattern = { "*" },
+  command = "redrawstatus",
+})
+--end decrease status line space
 
 -- start autosave
 -- local autosave = api.nvim_create_augroup("autosave", { clear = true })
--- nvim/lua/neo/core/options.lua
+++ nvim/lua/neo/core/options.lua
@@ -1,3 +1,4 @@
+vim.opt.cmdheight = 0
 vim.cmd("let g:netrw_liststyle = 3")
 
 -- showing realtive numbers in buffer
