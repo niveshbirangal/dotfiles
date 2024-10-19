db2932078094f23eee43d0160aa5ac76f0e16756
Nivesh Birangal <niveshbirangal@gmail.com>
2024-10-20 02:31:55 +0530

    add plugins cloak harpoon multiline render

---
+++ nvim/lua/neo/plugins/multiline.lua
@@ -0,0 +1,8 @@
+return {
+  "mg979/vim-visual-multi",
+  config = function()
+    vim.g.VM_maps["Find Under"] = "<C-d>"
+    vim.g.VM_maps["Find Subword Under"] = "<C-d>"
+    vim.g.theme = "codedark"
+  end,
+}
