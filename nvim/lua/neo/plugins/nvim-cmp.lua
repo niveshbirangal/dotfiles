ee1590bfc7fe563a9a5a9eac04f5fdae5251b0a2
Nivesh Birangal <niveshbirangal@gmail.com>
2024-10-27 01:09:46 +0530

    add custom snippets

--- nvim/lua/neo/plugins/nvim-cmp.lua
+++ nvim/lua/neo/plugins/nvim-cmp.lua
@@ -24,7 +24,8 @@ return {
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
-
+    require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/neo/snippets" } })
+    -- require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./my-snippets" } })
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
@@ -45,7 +46,7 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp"},
+        { name = "nvim_lsp" },
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
