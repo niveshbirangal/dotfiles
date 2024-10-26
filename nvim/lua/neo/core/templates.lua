d0eac8e25bbd0ff94cbfaebf3fffe051b28f3ecb
Nivesh Birangal <niveshbirangal@gmail.com>
2024-10-27 01:04:25 +0530

    remove templates

--- nvim/lua/neo/core/templates.lua
+++
@@ -1,5 +0,0 @@
-local keymap = vim.keymap
--- shortcuts
--- keymap.set("i", "clog", "console.log()")
-keymap.set("i", "CL", "console.log()<ESC>h", { desc = "console.log shortcut" })
-keymap.set("i", "CF", "function temp(){}<ESC>h", { desc = "console.log shortcut" })
