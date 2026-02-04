3ad66e0f5565a139947f6bddff4e719a76779400
Nivesh Birangal <niveshbirangal@gmail.com>
2026-02-04 10:28:23 +0530

    chore: add env.lua file pattern to cloak

--- nvim/lua/neo/plugins/cloak.lua
+++ nvim/lua/neo/plugins/cloak.lua
@@ -19,7 +19,7 @@ return {
         {
           -- Match any file starting with '.env'.
           -- This can be a table to match multiple file patterns.
-          file_pattern = ".env*",
+          file_pattern = { ".env*", "env.lua" },
           -- Match an equals sign and any character after it.
           -- This can also be a table of patterns to cloak,
           -- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
